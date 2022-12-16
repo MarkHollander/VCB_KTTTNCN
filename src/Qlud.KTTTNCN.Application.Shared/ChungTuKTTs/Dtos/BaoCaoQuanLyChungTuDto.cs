bb750da0a99a35b311687e00607faad7756aadbd
VUDM.HO <VUDM.HO@VUDM-VCB>
2022-12-16 17:27:51 +0700

    [BRD-21360] Tao DTO quan ly chung tu
        COMMIT DATE
            13 December 2022
    
        SUMMARY
           Tao DTO quan ly chung tu
    
        DESCRIPTION
    
        NOTE
    
        ISSUE
    
        TODO

---
+++ src/Qlud.KTTTNCN.Application.Shared/ChungTuKTTs/Dtos/BaoCaoQuanLyChungTuDto.cs
@@ -0,0 +1,18 @@
+﻿using Abp.Application.Services.Dto;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Qlud.KTTTNCN.ChungTuKTTs.Dtos
+{
+    public class BaoCaoQuanLyChungTuDto: EntityDto<long>
+    {        
+        public int SoThuTu { get; set; }
+        public string MauSo { get; set; }
+        public string KyHieu { get; set; }
+        public string SoChungTu { get; set; }
+        public string MaSoThue { get; set; }
+        public string HoVaTen { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ src/Qlud.KTTTNCN.Application.Shared/ChungTuKTTs/Dtos/GetChungTuInput.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Qlud.KTTTNCN.ChungTuKTTs.Dtos
+{
+    public class GetChungTuInput
+    {
+        public string SoChungTu { get; set; }
+        public string MaSoThue { get; set; }
+        public string HoVaTen { get; set; }
+        public string Status { get; set; }
+        public DateTime? NgayLap { get; set; }
+        public DateTime? NgayDuyet { get; set; }
+    }
+}
